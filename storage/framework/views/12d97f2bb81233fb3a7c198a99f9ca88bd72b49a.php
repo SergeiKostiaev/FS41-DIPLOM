a567ea014f272e7c86198531cff37b46e4317488
KostiaevSV <skprod.info@gmail.com>
2023-03-21 15:51:59 +0400

    other

---
+++ storage/framework/views/12d97f2bb81233fb3a7c198a99f9ca88bd72b49a.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ИдёмВКино</title>
+    <link href="<?php echo e(mix('css/normalize.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(mix('css/styles.css')); ?>" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&amp;subset=cyrillic,cyrillic-ext,latin-ext"
+        rel="stylesheet">
+</head>
+<body>
+<div id="root"></div>
+<script src="<?php echo e(mix('js/index.js')); ?>"></script>
+</body>
+</html>
+<?php /**PATH D:\OpenServer5.4.3\OSPanel\domains\fs-diplom-main\resources\views/welcome.blade.php ENDPATH**/ ?>
