98eb4a7f71593c99308f448d48f8ffde2b782929
KostiaevSV <skprod.info@gmail.com>
2023-03-21 15:19:48 +0400

    initial code

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SessionRequest;
+use App\Models\Session;
+use DateTime;
+use Illuminate\Http\Response;
+
+class SessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Session::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(SessionRequest $request)
+    {
+        return Session::create($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Session $session
+     * @return \Illuminate\Http\Response
+     */
+    public function show($datetime)
+    {
+        $timeSeance = DateTime::createFromFormat('Y-m-d', $datetime)->format('Y-m-d');
+        return Session::whereDate('datetime', $timeSeance)->get();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Session $session
+     * @return \Illuminate\Http\Response
+     */
+    public function update(SessionRequest $request, Session $session)
+    {
+        $session->fill($request->validated());
+        return $session->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Session $session
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Session $session)
+    {
+        if ($session->delete()) {
+            return response(null, Response::HTTP_NO_CONTENT);
+        }
+        return null;
+    }
+}
---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TicketRequest;
+use App\Models\Seat;
+use App\Models\Ticket;
+use Illuminate\Http\Response;
+
+class TicketController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TicketRequest $request)
+    {
+        $ticket = Ticket::create($request->validated());
+        foreach ($request->validated()['seats'] as $seatId) {
+            $seat = Seat::findOrFail($seatId);
+            $ticket->seats()->save($seat);
+        }
+        return response($ticket->whereId($ticket->id)->with('session')->with('seats')->first(), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Ticket $ticket
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Ticket::whereId($id)->with('session')->with('seats')->first();
+    }
+}
